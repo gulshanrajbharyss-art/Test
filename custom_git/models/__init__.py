536e1e7ee1515339ba06d15cc378f424836ccb9d
Gulshan Rajbhar <info@yottasoftwaresolutions.com>
2026-06-23 13:05:06 +0530

    Remove folder_name

--- custom_git/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2024-Today
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import git_repository
-from . import git_branch
-from . import git_commit
-from . import git_commit_file
-from . import git_local_ops
--- custom_git/services/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (C) 2024-Today
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import github_api
-from . import local_git
